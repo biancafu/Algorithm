c5243c063d316d91b0626c5fcaf0b52e99fb1664
Bianca Fu <0509biancafu@gmail.com>
2023-03-27 16:29:04 -0600

    circular linkedlist setup

---
+++ Data_Structures/Circular_Linked_Lists/circular_linkedlist.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Circular_LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    
