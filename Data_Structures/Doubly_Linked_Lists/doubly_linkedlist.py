fbbfa40d4059a3f86197e26717a42ea9fbf7f054
Bianca Fu <0509biancafu@gmail.com>
2023-04-01 13:28:36 -0600

    doubly linkedlist - append

--- Data_Structures/Doubly_Linked_Lists/doubly_linkedlist.py
+++ Data_Structures/Doubly_Linked_Lists/doubly_linkedlist.py
@@ -8,4 +8,20 @@ class DoublyLinkedList():
     def __init__(self) -> None:
         self.head = None
     
-    
+    #append
+    def append(self, data):
+        #2 cases: if list is empty, if list is not
+        #case 1: list is empty
+        if not self.head: 
+            self.head = Node(data)
+
+        #case 2: list not empty
+        else:
+            cur = self.head
+            while cur.next: #stop on last node
+                cur = cur.next
+            new_node = Node(data)
+            cur.next = new_node
+            new_node.prev = cur
+            
+
