72ae569cb948826e830989b59b34297298abe0d0
Bianca Fu <0509biancafu@gmail.com>
2023-04-13 13:52:32 -0600

    binarytree - preorder traversal

--- Data_Structures/Binary_Trees/Binary_Trees.py
+++ Data_Structures/Binary_Trees/Binary_Trees.py
@@ -7,3 +7,19 @@ class Node():
 class BinaryTree():
     def __init__(self, root) -> None:
         self.root = Node(root)
+    
+#traverse algorithms
+#DFS
+#pre-order: data -> left -> 
+#picture: F B A D C E G I H
+def preorder_print(self, start, traversal):
+    if not start:
+        return
+    traversal += str(start) + "-" #updated current traversal
+    traversal = self.preorder_print(start.left, traversal) #call function (recursion) with new traversal
+    traversal = self.preorder_print(start.right, traversal) #call function (recursion) with new traversal (after left was traversed)
+    return traversal
+    
+#in-order: left -> data -> right
+
+#post-order
