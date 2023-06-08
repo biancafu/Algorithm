e9b88ce9e1d63948e8163b1db69cd3f663a457d7
Bianca Fu <0509biancafu@gmail.com>
2023-06-08 16:08:25 -0600

    Binary Search - fixed_number

--- Data_Structures/Binary_Search/fixed_num.py
+++ Data_Structures/Binary_Search/fixed_num.py
@@ -1,6 +1,10 @@
 #find the fixed number: the number where index  == arr[index] using binary search
-A1 = [-10, -5, -3, 0, 1, 2, 3, 7]
-
+#########################################################################################################
+#if index > item: left of the index will not satisfy 
+#(since even if you subtract the least number it will still -1 and it will be same as index which means we are travelling parallelly)
+#same applies to the right side when index < item
+#THIS ONLY APPLIES BECAUSE IT IS AN ORDERED LIST WITH NO REPEATED NUMBERS
+#only when mid == A[mid] would make the rest of the sides possible to satisfy the condition as well
 
 def find_fixed_num_linear(A):
     for index, item in enumerate(A):
@@ -8,4 +12,31 @@ def find_fixed_num_linear(A):
             return index
     return None
 
-def find 
+def find_fixed_num(A):
+    low = 0
+    high = len(A) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if A[mid] > mid: 
+            high = mid - 1
+        elif A[mid] < mid:
+            low = mid + 1
+        elif A[mid] == mid: #else:
+            return mid
+    return None
+
+# Fixed point is 7:
+A1 = [-10, -5, -3, 0, 1, 2, 3, 7]
+
+# Fixed point is 0:
+A2 = [0, 2, 5, 8, 17]
+
+# No fixed point. Return "None":
+A3 = [-10, -5, 3, 4, 7, 9]
+print(A1)
+print(find_fixed_num(A1))
+print(A2)
+print(find_fixed_num(A2))
+print(A3)
+print(find_fixed_num(A3))
