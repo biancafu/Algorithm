4871f19ec1b0e52abc1c87a6c05294e840985f66
Bianca Fu <0509biancafu@gmail.com>
2023-06-08 17:05:57 -0600

    Binary Search - peak (from leatcode)

--- Data_Structures/Binary_Search/bitonic_peak.py
+++ Data_Structures/Binary_Search/bitonic_peak.py
@@ -20,6 +20,36 @@ def find_bitonic_peak(A):
         elif A[mid] < left and A[mid] > right: #eliminate right half
             high = mid - 1
     return None
+
+def find_peak(A): #https://leetcode.com/problems/find-peak-element/description/
+    #unordered list, with repeatable numbers, find any peak (could have more than 1): a number greater than its left and right side
+    #this works with binary search since eventually we will find one peak when we keep eliminating the side where it satisfies the condition (mid > right side/left side) 
+    #since then it means it is the current peak from that side, and we need a bigger peak from the other side
+    #when only 1 element
+    if len(A) == 1: return 0
+    #if first element/last element is peak: (since there can be multiple peaks)
+    if A[0] > A[1]: return A[1]
+    if A[len(A) - 1] > A[len(A) - 2]: return A[len(A) - 1]
+
+    #we can eliminate top and end since we checked it already
+    low = 1
+    high = len(A) - 2
+
+    while low <= high:
+        mid = (low + high) // 2
+        if A[mid] > A[mid - 1] and A[mid] > A[mid + 1]:
+            return A[mid]
+        elif A[mid] < A[mid - 1]: # we are only checking one side, since there can be multiple peaks and we do not need  worry if the other side also has peak
+            high = mid - 1
+        elif A[mid] < A[mid + 1]:
+            low = mid + 1
+    return None 
+
+            
+            
+
+
+
       
 
 # Peak element is "5".
