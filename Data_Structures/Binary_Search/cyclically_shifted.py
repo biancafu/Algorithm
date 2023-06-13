8f38c92025b3a19d465f10d3f8be3945e6d2bb1c
Bianca Fu <0509biancafu@gmail.com>
2023-06-12 19:41:47 -0600

    Binary Search - first entry with duplicates

--- Data_Structures/Binary_Search/cyclically_shifted.py
+++ Data_Structures/Binary_Search/cyclically_shifted.py
@@ -1,1 +1,17 @@
-#cyclicall shifted arrays, find the smallest number index
+def find(A):
+    low = 0
+    high = len(A) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if A[mid] > A[high]:
+            low = mid + 1
+        elif A[mid] <= A[high]:
+            high = mid 
+
+    return low
+
+A = [4, 5, 1, 2, 3]
+idx = find(A)
+print(A[idx])
--- Data_Structures/Binary_Search/first_entry.py
+++ Data_Structures/Binary_Search/first_entry.py
@@ -1,1 +1,24 @@
 #given a sorted list and key, return the index of the first occurence of the key value 
+def first_occurence(A, target):
+    low = 0
+    high = len(A) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if A[mid] == target:
+            if mid - 1 < 0: #make sure its within range
+                return mid
+            if A[mid - 1] != target: #if this is first occurence
+                return mid
+            high = mid - 1 #if this is not first occurence, continue to find
+        elif A[mid] > target:
+            high = mid - 1
+        elif A[mid] < target:
+            low = mid + 1
+    
+    return None
+
+A = [-14, -10, 2, 108, 108, 243, 285, 285, 285, 401]
+target = 108
+x = first_occurence(A, target)
+print(x)
