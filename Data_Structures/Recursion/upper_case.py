be054ea2443b577be8e62269a83209319641cec0
Bianca Fu <0509biancafu@gmail.com>
2023-06-15 18:07:19 -0600

    Recursion - find upper case

--- Data_Structures/Recursion/upper_case.py
+++ Data_Structures/Recursion/upper_case.py
@@ -1,4 +1,25 @@
 #Find the upper case in the string
 
-def find_uppercase(string, i = 0):
-    
+def find_uppercase(input, i = 0):
+    if input[i].isupper():
+        return input[i]
+    if i == len(input) - 1:
+        return "No upper case"
+    return find_uppercase(input, i + 1)
+
+#or 
+    # if i == len(input):
+    #     return "No upper case"
+    # if input[i].isupper():
+    #     return input[i]
+    # else:
+    #     return find_uppercase(input, i + 1)
+
+
+input_str_1 = "lucidProgramming"
+input_str_2 = "LucidProgramming"
+input_str_3 = "lucidprogramming"
+
+print(find_uppercase(input_str_1))
+print(find_uppercase(input_str_2))
+print(find_uppercase(input_str_3))
