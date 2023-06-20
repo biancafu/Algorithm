f9cab5b799faad187c15c849d373ee0566a51abe
Bianca Fu <0509biancafu@gmail.com>
2023-06-19 21:10:36 -0600

    Arrays&Hashing - group anagram modified

--- NeetCode/Arrays&Hashing/group_anagram.py
+++ NeetCode/Arrays&Hashing/group_anagram.py
@@ -8,19 +8,30 @@ from collections import defaultdict
 #empty key will assign empty default(set, list, or 0 for int)
 
 
+#modified solution from neetcode, faster runtime but time complexity is O(m*nlogn)
 class Solution(object):
     def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        result = defaultdict(list) #default value will be list for this map
+        result = defaultdict(list)
         for string in strs:
-            occurrence = [0] * 26 # a list of occurence of characters for this string
-
-            for char in string:
-                occurrence[ord(char) - ord("a")] += 1 #index: a = 0, b = 1...
-            
-            result[tuple(occurrence)].append(string) #use tuple cause list cannot be key, but tuples can since they are immutable
-
+            sorted_string = sorted(string)
+            result["".join(sorted_string)].append(string)
+        
         return result.values()
+
+#solution from neetcode
+# class Solution(object): O(n*m)
+#     def groupAnagrams(self, strs):
+#         """
+#         :type strs: List[str]
+#         :rtype: List[List[str]]
+#         """
+#         result = defaultdict(list) #default value will be list for this map
+#         for string in strs:
+#             occurrence = [0] * 26 # a list of occurence of characters for this string
+
+#             for char in string:
+#                 occurrence[ord(char) - ord("a")] += 1 #index: a = 0, b = 1...
+            
+#             result[tuple(occurrence)].append(string) #use tuple cause list cannot be key, but tuples can since they are immutable
+
+#         return result.values()
