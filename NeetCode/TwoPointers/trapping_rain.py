71d44a629d7333564c6722bbbe7ca9fcc7885d8d
Bianca Fu <0509biancafu@gmail.com>
2023-06-29 00:46:07 -0600

    two pointers - trapping rain slower solution

--- NeetCode/TwoPointers/trapping_rain.py
+++ NeetCode/TwoPointers/trapping_rain.py
@@ -1,6 +1,24 @@
 def trap( height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
+    #slower solution
+    #O(n) = O(3n)
+    leftmax = []
+    rightmax = []
+    rain = 0
+
+    maxnum = 0
+    for i in range(0, len(height)):
+        leftmax.append(maxnum)
+        maxnum = max(maxnum, height[i])
+    
+    maxnum = 0
+    for i in range(len(height) - 1, -1, -1):
+        rightmax.insert(0, maxnum)
+        maxnum = max(maxnum, height[i])
+
+    for i in range(1, len(height) - 1):
+        area = min(leftmax[i], rightmax[i]) - height[i]
+        if area > 0:
+            rain += area
+    
+    return rain
         
