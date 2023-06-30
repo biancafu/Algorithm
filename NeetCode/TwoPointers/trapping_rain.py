b63c663be0e9afc7fd00ee2aa7ddf92e354878ba
Bianca Fu <0509biancafu@gmail.com>
2023-06-30 00:22:46 -0600

    two pointers - trap rain faster solution and neetcode solution with explanation

--- NeetCode/TwoPointers/trapping_rain.py
+++ NeetCode/TwoPointers/trapping_rain.py
@@ -1,12 +1,53 @@
+def trap_neetcode(height):
+    #different incrementation
+    l, r = 0, len(height) - 1 
+    lmax, rmax = height[l], height[r]
+    total = 0
+    while l < r:
+        if lmax < rmax: 
+            l += 1 #increment to the next value first since we have our lmax and rmax set as the start and end value already
+            #if we update the max first, our area will never be negatvie
+            #this works because even if max is updated (to the current height), we will get 0 from subtraction
+            #meaning that no area will be added
+            lmax = max(lmax, height[l])
+            total += lmax - height[l]
+        else: #rmax < lmax
+            r -= 1
+            rmax = max(rmax, height[r])
+            total += rmax - height[r]
+            
+
+    return total
+
 def trap_faster(height):
-     #to calculate the trapped rain area use logic:
+    #to calculate the trapped rain area use logic:
     #min(left max height, right max height) - current height
     #shift the lower max height side
     #we only know one side max of the current position, so how come this works?
     #because we want the min of the max, so we just need the smaller side
-    l, r = 0, len(height) - 1
 
-    pass
+    #concern that how do we know if the side will close off if we only know one side of max height?
+    #we move the side with smaller max height, so we know for sure, the other side max >= this side 
+    
+
+    l, r = 1, len(height) - 2 #skipping the first and last value since end point cannot contain water
+    lmax, rmax = height[0], height[len(height) - 1]
+    total = 0
+    while l < r: 
+        if lmax <= rmax: 
+            if lmax - height[l] > 0:
+                total += lmax - height[l] #lmax is the smaller side
+            lmax = max(lmax, height[l])
+            l += 1
+        else: #rmax < lmax
+            if rmax - height[r] > 0:
+                total += rmax - height[r]
+            rmax = max(rmax, height[r])
+            r -= 1
+
+    return total
+
+
 def trap(height):
     #slower solution
     #O(n) = O(3n)
