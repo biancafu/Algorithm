2752b4bc0bc7c019d6d6c134ea5dafbce793e630
Bianca Fu <0509biancafu@gmail.com>
2023-07-10 22:50:48 -0600

    binary search - find min

---
+++ NeetCode/BinarySearch/find_min.py
@@ -0,0 +1,13 @@
+#wasn't able to solve it, already done it before as well, didn't understand concept fully
+
+class Solution(object):
+    def findMin(self, nums):
+        low = 0
+        high = len(nums) - 1
+        while low < high: #exclusive becuase when low == high, our increment will be high = mid which means it will become infinite loop since we are not incrementing
+            mid = (low + high) // 2
+            if nums[mid] > nums[high]: #the right half contains the lower number (restarted somewhere)
+                low = mid + 1
+            else: #if mid <= high means we are ascending, so go left half for smaller values, but we have to include mid because it could be the lowest number, therefore high = mid
+                high = mid 
+        return nums[low]
