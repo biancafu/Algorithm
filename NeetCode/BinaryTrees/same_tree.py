9c998997d633eefe98e1799ac3880cdcb9f0b832
Bianca Fu <0509biancafu@gmail.com>
2023-08-19 16:22:04 -0600

    binary tree - same tree

---
+++ NeetCode/BinaryTrees/same_tree.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isSameTree(self, p, q):
+        if not q and not p:
+            return True
+        if not q or not p or p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        
