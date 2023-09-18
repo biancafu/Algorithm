81716eae29087b0bf16bd57fbefe9855b4b4aa0d
Bianca Fu <0509biancafu@gmail.com>
2023-09-17 23:52:38 -0600

    graphs - island dfs

--- NeetCode/Graphs/islands.py
+++ NeetCode/Graphs/islands.py
@@ -1,7 +1,32 @@
 class Solution(object):
-    #bfs solution: 20% speed 86% memory
+    #dfs 95% speed 86% memory
+    def numIslands(self, grid):
+        m = len(grid)
+        n = len(grid[0])
+        islands = 0
+
+        def dfs(r,c):
+            if r < 0 or c < 0 or r >= m or c >= n or grid[r][c] != "1":
+                return
+            
+            grid[r][c] = "2"
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+
+        
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == "1":
+                    dfs(r,c)
+                    islands += 1
+
+        return islands
+
+    #bfs solution: 20% speed 86% memory O(m*n) becuz we are skipping the ones we checked already essentially it will be around O(m*n)
     #instead of using a set to keep track of visited coords, just change the grid value to something else to keep track
-    
+
     def numIslands(self, grid):
         
         m = len(grid)
