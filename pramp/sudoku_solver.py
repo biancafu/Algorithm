2d117a7488ca78065f8e347137e59194518c019e
Bianca Fu <0509biancafu@gmail.com>
2023-09-20 18:04:09 -0600

    pramps interview - sudoku solver

--- pramp/sudoku_solver.py
+++ pramp/sudoku_solver.py
@@ -5,43 +5,69 @@
 #was on the right track, but got confused as i didn't think straight, i thought if we had to go through all possible solution will be to inefficient
 #thought there was a "smarter/faster" to solve
 
+#93% speed, 90% memory
+from collections import defaultdict, deque
+class Solution(object):
+    def solveSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        PAY ATTENTION TO DETAILS! (made 2 mistakes: first didn't notice the board is made of strings not numbers, 
+        second when i copy and paste, i messed up the condition (used 2 rows in stead of rows and columns))
+        """
+        #mistake: pay attention to details!! the "numbers" stored are not numbers, they are string!!
+        #use a hashset to store all existing numbers
+        rows = defaultdict(set)
+        columns = defaultdict(set)
+        blocks = defaultdict(set)
+        spaces = deque() #use a deque to store blank spaces so we can go back to previous step like the ones we do in backtracking (easier to append/pop)
 
-from collections import defaultdict,deque
-class Solution:
-    def solveSudoku(self, board: List[List[str]]) -> None:
-    
-        rows,cols,block,seen = defaultdict(set),defaultdict(set),defaultdict(set),deque([])
-        for i in range(9):
-            for j in range(9):
-                if board[i][j]!=".":
-                    rows[i].add(board[i][j])
-                    cols[j].add(board[i][j])
-                    block[(i//3,j//3)].add(board[i][j])
+        #add all existing numbers to the hashset
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    spaces.append((r,c))
                 else:
-                    seen.append((i,j))
-        
-        def dfs():
-            if not seen: #no board space
+                    rows[r].add(board[r][c])
+                    columns[c].add(board[r][c])
+                    blocks[(r//3,c//3)].add(board[r][c])
+
+
+        def dfs(): #use this recursie function to check all possible solution for blank spaces(deque)
+            #base condition when its a possible solution:
+            if not spaces: #no more spaces to check (all the others passed)
                 return True
+
+            r,c = spaces[0] #don't use popleft here, this is because we will need to pop within the for loop for it to balance with the append when cases failed, and we are reverting.
+            #if we only have append, and we don't pop, we will end up appending repeating coordinates
+            #check a possible num to traverse down:
+            for num in {'1','2','3','4','5','6','7','8','9'}:
+                #we have repeated numbers, so skip
+                if num in rows[r] or num in columns[c] or num in blocks[(r//3,c//3)]:
+                    continue
+                #we found a possible number
+                board[r][c] = num
+                rows[r].add(num)
+                columns[c].add(num)
+                blocks[(r//3,c//3)].add(num)
+                spaces.popleft()
+
+                #if it succeeded, return True
+                if dfs(): 
+                    return True
+                #failed this num, we will remove everything and add the spot back to q to try other numbers   
+                else:
+                    board[r][c] = "."
+                    rows[r].remove(num)
+                    columns[c].remove(num)
+                    blocks[(r//3,c//3)].remove(num)
+                    spaces.appendleft((r,c))
             
-            r,c = seen[0] #the first empty spot
-            t = (r//3,c//3)
-            for n in {'1','2','3','4','5','6','7','8','9'}:
-                if n not in rows[r] and n not in cols[c] and n not in block[t]: #no one else this number yet
-                    board[r][c]=n #switch board spot to that number
-                    rows[r].add(n) #add them to the map tracking rows, columns and block
-                    cols[c].add(n)
-                    block[t].add(n)
-                    seen.popleft() #pop it off the seen (since its no longer a space)
-                    if dfs(): #check if this new board can find a solution
-                        return True #True if we can find one (by returning True from dfs call)
-                    else: #if not, revert back (change the number to something else)
-                        board[r][c]="."
-                        rows[r].discard(n)
-                        cols[c].discard(n)
-                        block[t].discard(n)
-                        seen.appendleft((r,c)) #put the empty spot back on the board
-            #if nothing works, then we can't find a solution
+            #if we gone through everything and dont return a True, means we failed everything so return False
             return False
         
         dfs()
+
+
+                    
+
