d2f29e2052f21822885a1273561c47ec02debabf
Bianca Fu <0509biancafu@gmail.com>
2023-12-18 22:56:24 -0700

    graph - currency arbitrage

---
+++ NeetCode/Graphs/currency_arbitrage.py
@@ -0,0 +1,7 @@
+#use Bellman Ford's algorithm
+
+#create a weighted graph using log(rate) as their weight
+
+#check through the currency with graph and if sum(log(rate)) > 0 means there is a arbitrage or < 0 means arbitrage (from other side i think)
+
+#if there is a negative weighted cycle -> bellman ford wont work
